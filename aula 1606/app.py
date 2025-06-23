10a09ffeae665246ede467c97d87bcd0cb778c02
guilherme <guilherme098321@gmail.com>
2025-06-23 17:06:05 -0300

    continuacao projeto formulario

--- aula 1606/app.py
+++ aula 1606/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,16 +7,22 @@ app = Flask(__name__)
 def profile(username):
 
     return render_template('profile.html', user=username)
+
 @app.route('/')
 
 def home():
 
-    produtos = ["Maça", "Banana", "Laranja"]
-
-    logado = True
-
-    return render_template("home.html", produtos=produtos, logado=logado)
+    return render_template("login.html")
 
 if __name__ == '__main__':
 
     app.run(debug=True)
+usuarios = ['guilherme' 'heloisa']
+senhas = [1, 2]
+@app.route('/logar', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    session['username'] = username
+    if username in usuarios and password in senhas:
+        return redirect(url_for('profile'))
