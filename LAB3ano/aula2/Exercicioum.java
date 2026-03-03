9ac4d5926e3a0898cd5c2e2efa3c7c7e58ad932f
Guilherme <guilhewrme098321@gmail.com>
2026-03-03 17:07:36 -0300

    exercicio de java da aula 2

---
+++ LAB3ano/aula2/Exercicioum.java
@@ -0,0 +1,12 @@
+public class Exercicioum {
+    public static void main(String[] args) {
+        String num = "10";
+        int numero = Integer.parseInt(num);
+
+        if (numero % 2 == 0) {
+            System.out.println(num);
+        } else {
+            System.out.println(num);
+        }
+    }
+}
